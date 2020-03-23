135b0b11a18391a2932642bd647edff6301507c9
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-03-23 19:33:57 -0400

    Update install_required_packages.sh

--- packer/scripts/install_required_packages.sh
+++ packer/scripts/install_required_packages.sh
@@ -2,4 +2,8 @@
 
 set -ex
 
-sudo apt-get update && sudo apt-get install -y unzip
+# https://github.com/hashicorp/packer/issues/2639
+timeout 180 /usr/bin/cloud-init status --wait
+
+sudo apt-get update
+sudo apt-get install -y unzip
