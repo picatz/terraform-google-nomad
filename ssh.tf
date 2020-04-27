6b753e86737bb0256c32ea9d922d31956d8b619c
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-04-27 00:12:58 -0400

    Update ssh.tf

--- ssh.tf
+++ ssh.tf
@@ -1,14 +1,19 @@
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
+  rsa_bits  = "2048"
 }
 
 resource "local_file" "ssh_public_key" {
+  count = var.save_ssh_keypair_locally ? 1 : 0
+
   content         = tls_private_key.ssh_key.public_key_openssh
   filename        = "bastion.pub"
   file_permission = "0600"
 }
 
 resource "local_file" "ssh_private_key" {
+  count = var.save_ssh_keypair_locally ? 1 : 0
+
   content         = tls_private_key.ssh_key.private_key_pem
   filename        = "bastion"
   file_permission = "0600"
