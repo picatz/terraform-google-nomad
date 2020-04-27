a6d616dd8bcc4a695c6e9a00dc1262faa737a6fb
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-04-27 00:12:46 -0400

    Create tls_ca.tf

---
+++ tls_ca.tf
@@ -0,0 +1,23 @@
+resource "tls_private_key" "nomad-ca" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_self_signed_cert" "nomad-ca" {
+  is_ca_certificate     = true
+  validity_period_hours = 87600
+
+  key_algorithm   = tls_private_key.nomad-ca.algorithm
+  private_key_pem = tls_private_key.nomad-ca.private_key_pem
+
+  subject {
+    common_name  = "nomad-ca.local"
+    organization = var.tls_organization
+  }
+
+  allowed_uses = [
+    "cert_signing",
+    "digital_signature",
+    "key_encipherment",
+  ]
+}
