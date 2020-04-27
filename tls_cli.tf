474415432c2f7d13e5535ac84d770040fb8c0bdd
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-04-27 00:12:54 -0400

    Create tls_cli.tf

---
+++ tls_cli.tf
@@ -0,0 +1,33 @@
+resource "tls_private_key" "nomad-cli" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "tls_cert_request" "nomad-cli" {
+  key_algorithm   = tls_private_key.nomad-cli.algorithm
+  private_key_pem = tls_private_key.nomad-cli.private_key_pem
+
+  ip_addresses = [
+    module.load_balancer.external_ip,
+    "127.0.0.1",
+  ]
+
+  subject {
+    common_name  = "client.global.nomad"
+    organization = var.tls_organization
+  }
+}
+
+resource "tls_locally_signed_cert" "nomad-cli" {
+  cert_request_pem = tls_cert_request.nomad-cli.cert_request_pem
+
+  ca_key_algorithm   = tls_private_key.nomad-ca.algorithm
+  ca_private_key_pem = tls_private_key.nomad-ca.private_key_pem
+  ca_cert_pem        = tls_self_signed_cert.nomad-ca.cert_pem
+
+  validity_period_hours = 87600
+
+  allowed_uses = [
+    "client_auth",
+  ]
+}
