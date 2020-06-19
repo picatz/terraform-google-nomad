54004c1349126411f2afd1fcc44123b33a6b1cfd
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-06-19 00:02:36 -0400

    Use format function with project name

--- vms.tf
+++ vms.tf
@@ -2,7 +2,7 @@ module "bastion" {
   source         = "./modules/vm"
   name           = "nomad-bastion"
   machine_type   = var.bastion_machine_type
-  image          = "my-nomad-cluster/nomad-bastion"
+  image          = format("%s/nomad-bastion", var.project)
   subnetwork     = module.nomad-network.subnetwork
   zone           = var.zone
   tags           = ["nomad-bastion"]
@@ -16,7 +16,7 @@ module "server" {
   instances      = var.server_instances
   name           = "nomad-server"
   machine_type   = var.server_machine_type
-  image          = "my-nomad-cluster/nomad-server"
+  image          = format("%s/nomad-server", var.project)
   subnetwork     = module.nomad-network.subnetwork
   zone           = var.zone
   tags           = ["nomad-server"]
@@ -31,7 +31,7 @@ module "client" {
   instances      = var.client_instances
   name           = "nomad-client"
   machine_type   = var.client_machine_type
-  image          = "my-nomad-cluster/nomad-client"
+  image          = format("%s/nomad-client", var.project)
   subnetwork     = module.nomad-network.subnetwork
   zone           = var.zone
   tags           = ["nomad-client"]
