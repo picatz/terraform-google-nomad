b1e2f425845a06aca33dff7ce3c264cbf799b286
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-07-11 18:06:47 -0400

    Update install_stack_driver_agent.sh

--- packer/scripts/install_stack_driver_agent.sh
+++ packer/scripts/install_stack_driver_agent.sh
@@ -2,4 +2,14 @@
 
 set -ex
 
-curl -s https://dl.google.com/cloudagents/install-logging-agent.sh | sudo bash
+# OLD WAY:
+# curl -s https://dl.google.com/cloudagents/install-logging-agent.sh | sudo bash
+
+# NEW WAY:
+# https://cloud.google.com/logging/docs/agent/installation#agent-install-debian-ubuntu
+curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+sudo bash add-logging-agent-repo.sh
+sudo apt-get update
+sudo apt-get install -y google-fluentd
+sudo apt-get install -y google-fluentd-catch-all-config-structured
+sudo systemctl enable google-fluentd
