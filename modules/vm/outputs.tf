4f339d61c95dec8cb6c1309ab7ee630dd367ab66
Kent 'picat' Gruber <kgruber1@emich.edu>
2020-07-13 18:45:17 -0400

    Fix missing outputs for instances equal to one, now greater than or equal to
    Pretty sure this could be simpler, but it work for now.

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -1,8 +1,8 @@
 output "external_ip" {
   # value is an empty string unless external_ip is true
-  value = (var.external_ip && var.instances > 1) ? google_compute_instance.vm[0].network_interface[0].access_config[0].nat_ip : ""
+  value = (var.external_ip && var.instances >= 1) ? google_compute_instance.vm[0].network_interface[0].access_config[0].nat_ip : ""
 }
 
 output "internal_ip" {
-  value = var.instances > 1 ? google_compute_instance.vm[0].network_interface[0].network_ip : ""
+  value = (var.instances >= 1) ? google_compute_instance.vm[0].network_interface[0].network_ip : ""
 }
