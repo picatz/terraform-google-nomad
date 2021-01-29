7c95d8f33848abd9aada34806a7f765b3ccec90c
Kent 'picat' Gruber <kent@hashicorp.com>
2021-01-29 18:24:20 -0500

    Use the random_id resource for gossip keys

--- startup_scripts.tf
+++ startup_scripts.tf
@@ -1,3 +1,8 @@
+# Note: Always escape potential forward-slashes in the the base64 output of the gossip key 
+#       if being passed to commands like sed in a startup-script. And this is obviously not perfect 
+#       for many reasons, but is required for the current configuration setup using a cloud-init
+#       startup script.
+
 data "template_file" "server_bootstrap_script" {
   template = file("${path.module}/templates/server.sh")
 
@@ -7,9 +12,9 @@ data "template_file" "server_bootstrap_script" {
     nomad_ca_cert              = tls_self_signed_cert.nomad-ca.cert_pem
     nomad_server_cert          = tls_locally_signed_cert.nomad-server.cert_pem
     nomad_server_private_key   = tls_private_key.nomad-server.private_key_pem
-    nomad_gossip_secret_key    = replace(base64encode(random_password.nomad-gossip-key.result), "/", "x")
+    nomad_gossip_secret_key    = replace(random_id.nomad-gossip-key.b64_std, "/", "\\/")
     nomad_acls_enabled         = var.nomad_acls_enabled
-    consul_gossip_secret_key   = replace(base64encode(random_password.consul-gossip-key.result), "/", "x")
+    consul_gossip_secret_key   = replace(random_id.consul-gossip-key.b64_std, "/", "\\/")
     consul_ca_cert             = tls_self_signed_cert.consul-ca.cert_pem
     consul_server_cert         = tls_locally_signed_cert.consul-server.cert_pem
     consul_server_private_key  = tls_private_key.consul-server.private_key_pem
@@ -36,7 +41,7 @@ data "template_file" "client_bootstrap_script" {
     nomad_client_cert          = tls_locally_signed_cert.nomad-client.cert_pem
     nomad_client_private_key   = tls_private_key.nomad-client.private_key_pem
     nomad_acls_enabled         = var.nomad_acls_enabled
-    consul_gossip_secret_key   = replace(base64encode(random_password.consul-gossip-key.result), "/", "x")
+    consul_gossip_secret_key   = replace(random_id.consul-gossip-key.b64_std, "/", "\\/")
     consul_ca_cert             = tls_self_signed_cert.consul-ca.cert_pem
     consul_client_cert         = tls_locally_signed_cert.consul-client.cert_pem
     consul_client_private_key  = tls_private_key.consul-client.private_key_pem
