be555d5fe1edeb86e6f42cc01d06e64609e3de7f
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2021-02-07 18:42:27 -0500

    Fix gvisor installation

--- packer/scripts/install_gvisor.sh
+++ packer/scripts/install_gvisor.sh
@@ -2,7 +2,19 @@
 
 set -ex
 
-curl -fsSL https://gvisor.dev/archive.key | sudo apt-key add -
-sudo add-apt-repository "deb https://storage.googleapis.com/gvisor/releases release main"
-sudo apt-get update && sudo apt-get install -y runsc
-sudo runsc install
+# https://gvisor.dev/docs/user_guide/install/
+ARCH=$(uname -m)
+URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
+wget ${URL}/runsc ${URL}/runsc.sha512 \
+  ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
+sha512sum -c runsc.sha512 \
+  -c containerd-shim-runsc-v1.sha512
+rm -f *.sha512
+chmod a+rx runsc containerd-shim-runsc-v1
+sudo mv runsc containerd-shim-runsc-v1 /usr/local/bin
+
+# install as a docker runtime
+sudo /usr/local/bin/runsc install
+
+# sudo systemctl reload docker
+# docker run --rm --runtime=runsc hello-world
