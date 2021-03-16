fd98f5ef8546e853743cd1139e4d971024098366
Victor Ng <victor@crankycoder.com>
2021-03-16 14:06:52 -0400

    Patched gvisor install to not use DEB repo
    The hashsum is not updated properly in the google repo for some reason

--- packer/scripts/install_gvisor.sh
+++ packer/scripts/install_gvisor.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 set -ex
-
-curl -fsSL https://gvisor.dev/archive.key | sudo apt-key add -
-sudo add-apt-repository "deb https://storage.googleapis.com/gvisor/releases release main"
-sudo apt-get update && sudo apt-get install -y runsc
-sudo runsc install
+ARCH=$(uname -m)
+URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
+wget ${URL}/runsc ${URL}/runsc.sha512 ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
+sha512sum -c runsc.sha512 -c containerd-shim-runsc-v1.sha512
+rm -f *.sha512
+chmod a+rx runsc containerd-shim-runsc-v1
+sudo mv runsc containerd-shim-runsc-v1 /usr/local/bin
