15afaec66b4213dd71666089b1fbd6f0e9f91e10
Kent 'picat' Gruber <kent@hashicorp.com>
2021-04-03 19:17:07 -0400

    Add client load balancer to serve Grafana via Traefik + Consul + Envoy
    $ make consul/metrics/acls
    $ consul_acl_token=... make nomad/metrics
    $ nomad run jobs/ingress/traefik.hcl

--- load_balancer.tf
+++ load_balancer.tf
@@ -9,3 +9,15 @@ module "load_balancer" {
   instances         = formatlist("${format("%s-%s/server", var.region, var.zone)}-%d", range(var.server_instances))
   and_depends_on    = [module.network]
 }
+
+module "grafana_load_balancer" {
+  source            = "./modules/load-balancer"
+  region            = var.region
+  name              = "grafana-load-balancer"
+  ports             = [3000]
+  health_check_port = 3000
+  target_tags       = ["client"]
+  network           = module.network.name
+  instances         = formatlist("${format("%s-%s/client", var.region, var.zone)}-%d", range(var.client_instances))
+  and_depends_on    = [module.network]
+}
