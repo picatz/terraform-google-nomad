68a7ce381582b372b745e5b481566828ccd9d46c
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2021-05-31 09:38:48 -0400

    Fix falco install script

--- packer/scripts/install_falco.sh
+++ packer/scripts/install_falco.sh
@@ -3,7 +3,7 @@
 set -ex
 
 curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | sudo apt-key add -
-echo "deb https://dl.bintray.com/falcosecurity/deb stable main" | sudo tee -a /etc/apt/sources.list.d/falcosecurity.list
+echo "deb https://download.falco.org/packages/deb stable main" | sudo tee -a /etc/apt/sources.list.d/falcosecurity.list
 sudo apt-get update -y
 sudo apt-get -y install linux-headers-$(uname -r)
 sudo apt-get install -y falco
